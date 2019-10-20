099aaadf9a87d3e26cb9cd0fad6ad6ec0b797805
hcs <hslayer@ucsc.edu>
2019-10-20 15:25:30 -0800

    Deployed  with MkDocs version: 1.0.4

---
+++ scripts/katex.js
@@ -0,0 +1,11 @@
+/* Configures KaTeX for $ and $$ delimiters */ 
+document.addEventListener("DOMContentLoaded", function() {
+  renderMathInElement(document.body, {
+      delimiters: [
+          {left: "$$", right: "$$", display: true},
+          {left: "\\[", right: "\\]", display: true},
+          {left: "$", right: "$", display: false}
+      ]
+  });
+});
+
